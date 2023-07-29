34a39766c6fb645d34c78ae136e0592b87f646b9
semahkadri <semah.kadri@esprit.tn>
2023-07-29 19:15:33 +0100

    Update Settings 📌

--- djangocrud/asgi.py
+++ djangocrud/asgi.py
@@ -3,7 +3,7 @@ ASGI config for djangocrud project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, visit
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
